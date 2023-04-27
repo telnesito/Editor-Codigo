c4fc437408c4adcc812e113853a4685c2d93d31b
Carlos <carlosj_1703@hotmail.com>
2023-04-27 18:19:50 -0400

    fix-login

--- src/componentes/auth/login/Login.jsx
+++ src/componentes/auth/login/Login.jsx
@@ -19,13 +19,14 @@ const Login = () => {
 
     <Box bgcolor={'white'} display={'flex'} flexDirection={'column'} alignItems={'center'} justifyContent={'center'} height={'100vh'} minHeight={'700px'}>
 
-      <Box display={'flex'} flexDirection={'column'} alignItems={'center'} width={'85%'} height={'80%'}>
+      <Box display={'flex'} flexDirection={'column'} alignItems={'center'} width={'100%'} >
 
-        <img width={'200px'} src='/img/imglogin.png'></img>
+        <img width={'300px'} src='/img/img-login.jpg'></img>
         <Typography color={'#415A77'} fontSize={'25px'} fontWeight={700} paddingBottom={'5px'} width={'80%'} textAlign={'center'}>Welcome to Codesue</Typography>
         <Typography color={'gray'}>Keep your data safe!</Typography>
         <form className='c-form' onSubmit={formik.handleSubmit}>
           <TextField
+            fullWidth
             label='Email'
             name='email'
             id='email'
@@ -49,10 +50,10 @@ const Login = () => {
 
           <Button sx={{ mt: '30px', mb: '10px', height: '50px', fontWeight: '700' }} variant='contained' type='submit'>Ingresar</Button>
         </form>
-        <Typography fontSize={'15px'} fontWeight={700} color={'#415A77'} textAlign={'center'}>Forgot your password?</Typography>
       </Box>
 
-      <Box display={'flex'} gap={'5px'}>
+      <Typography fontSize={'15px'} fontWeight={700} color={'#415A77'} textAlign={'center'}>Forgot your password?</Typography>
+      <Box display={'flex'} height={'40px'} alignItems={'end'} gap={'5px'}>
         <Typography component={'div'} color={'gray'}>Dont have an account?</Typography>
         <Typography sx={{ cursor: 'pointer' }} onClick={() => navigate('/')} fontWeight={700} color={'#415A77'}>Register!</Typography>
 
