ff31d507247481eb7b33770d01a84e59b5d71496
Telnesito <carlosj_1703@hotmail.com>
2023-06-03 21:01:16 -0400

    adminUsers-filteredList

--- services/auth.js
+++ services/auth.js
@@ -185,4 +185,31 @@ const recuperarClave = async (data = {}) => {
   }
 }
 
-export const auth = { recuperarClave, verificarCorreo, iniciarSesion, crearCuenta, actualizarClave, getProfile, deleteUser, cerrarSesion }
+
+
+const obtenerUsuarios = async () => {
+  try {
+    const response = await fetch(`${URL}/get-all-users`, {
+      method: 'GET', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    })
+
+    return response.json()
+  } catch (error) {
+    console.log(error)
+
+  }
+}
+
+
+
+
+export const auth = { obtenerUsuarios, recuperarClave, verificarCorreo, iniciarSesion, crearCuenta, actualizarClave, getProfile, deleteUser, cerrarSesion }
--- src/componentes/AdminView/AdminUsers.jsx
+++ src/componentes/AdminView/AdminUsers.jsx
@@ -1,8 +1,188 @@
-import { Box } from '@mui/material'
+import { Box, Button, Menu, MenuItem, Card, CardActions, CardContent, IconButton, Typography, Divider, TextField } from '@mui/material'
+import { auth } from '../../../services/auth'
+
+import { useEffect, useState } from 'react'
+import { MoreVert, Verified, Password, Email, Warning, Error } from '@mui/icons-material'
 
 const AdminUsers = () => {
+  const [usuarios, setUsuarios] = useState([])
+  const [filterUser, setFilterUser] = useState("")
+  const [listUserFiltered, setListUserFiltered] = useState([])
+
+  useEffect(() => {
+    const obtenerUsuarios = async () => {
+      try {
+        const response = await auth.obtenerUsuarios()
+        setUsuarios(response)
+        console.log(response)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    obtenerUsuarios()
+  }, [])
+
+  // useEffect(() => {
+
+  //   const filtered = listProjects.filter((project) => project.nombre.toLowerCase().includes(searchText.toLowerCase()))
+  //   setFilteredList(filtered)
+  // }, [searchText, listProjects])
+
+  const handleFilterUser = (text) => {
+    setFilterUser(text)
+  }
+
+  useEffect(() => {
+
+    const filtered = usuarios.filter((user) => user.email.toLowerCase().includes(filterUser.toLowerCase()))
+
+    setListUserFiltered(filtered)
+
+
+  }, [filterUser, usuarios])
+
+
+  const [anchorEl, setAnchorEl] = useState({}); // Estado separado para cada elemento del mapeo
+
+  const handleClick = (event, index) => { // Pasar el índice del elemento al hacer clic
+    setAnchorEl(prevState => ({
+      ...prevState,
+      [index]: event.currentTarget // Usar el índice como clave para almacenar la referencia del elemento
+    }));
+  };
+
+  const handleClose = (index) => { // Pasar el índice del elemento al cerrar
+    setAnchorEl(prevState => ({
+      ...prevState,
+      [index]: null // Restablecer la referencia del elemento al cerrar el menú
+    }));
+  };
+
   return (
-    <Box>AdminUsers</Box>
+    <Box
+      display={'flex'}
+      flexDirection={'column'}
+      height={'auto'}
+      alignItems={'center'}
+      justifyContent={'center'}
+      marginTop={'24px'}
+    >
+
+      <Box width={'95%'}>
+        <Typography variant='h5' textAlign={'left'} fontWeight={700}>Usuarios registrados</Typography>
+
+        <TextField value={filterUser} onChange={({ target }) => handleFilterUser(target.value)} sx={{ width: '300px' }} type='text' placeholder='Ingrese un email para filtrar' label='Busqueda rapida' variant='standard'></TextField>
+      </Box>
+      <Box
+        display={'flex'}
+        flexDirection={'row '}
+        width={'100%'}
+        height={'auto'}
+        flexWrap={'wrap'}
+        gap={'25px'}
+        alignItems={'center'}
+        justifyContent={'center'}
+        marginTop={'24px'}
+      >
+
+        {listUserFiltered.length === 0 ? <Typography width={'95%'}>No hay usuarios que mostrar</Typography> : listUserFiltered.map(({ uid, email, state }, index) =>
+          <Card
+            elevation={3}
+            sx={{ width: '30%', minWidth: '300px' }}
+            key={index}
+          >
+            <CardContent
+              sx={{ display: 'flex', justifyContent: 'center' }}
+            >
+              <Box
+                flexDirection={'column'}
+                justifyContent={'center'}
+                alignItems={'left'}
+                display={'flex'}
+                height={'125px'}
+                gap={'3px'}
+                width={'100%'}
+              >
+                <Menu
+                  sx={{
+                    mt: '10px'
+                  }}
+                  anchorEl={anchorEl[index]} // Usar la referencia almacenada para cada elemento
+                  open={Boolean(anchorEl[index])} // Comprobar si la referencia existe
+                  onClose={() => handleClose(index)} // Pasar el índice al cerrar el menú
+                >
+                  <MenuItem disabled={state}>
+                    <Box display={'flex'} alignItems={'center'} gap={'10px'}>
+                      <Verified color='primary' fontSize='small' />
+                      <Typography variant='body2'>
+                        Verificar
+                      </Typography>
+                    </Box>
+                  </MenuItem>
+                  <Divider />
+
+                  <MenuItem>
+                    <Box display={'flex'} alignItems={'center'} gap={'10px'}>
+                      <Password color='primary' fontSize='small' />
+                      <Typography variant='body2'>
+                        Cambiar clave
+                      </Typography>
+                    </Box>
+                  </MenuItem>
+                  <Divider />
+                  <MenuItem>
+                    <Box display={'flex'} alignItems={'center'} gap={'10px'}>
+                      <Email color='primary' fontSize='small' />
+                      <Typography variant='body2'>
+                        Cambiar email
+                      </Typography>
+                    </Box>
+                  </MenuItem>
+                </Menu>
+                <Typography fontWeight={'700'} variant='body'>Email</Typography>
+                <Typography variant='body2'>{email}</Typography>
+                <Typography fontWeight={'700'} variant='body'>UID</Typography>
+                <Typography variant='body2'>{uid}</Typography>
+                <Typography variant='body' fontWeight={700}>Estado</Typography>
+                {state ?
+                  <Box display={'flex'} gap={'5px'} alignItems={'center'}>
+
+                    <Typography variant='body2'>Verificado</Typography>
+                    <Verified fontSize='small' color='success' />
+                  </Box>
+
+                  :
+                  <Box display={'flex'} gap={'5px'} alignItems={'center'}>
+                    <Typography>No verificado</Typography>
+                    <Error fontSize='small' color='error'></Error>
+                  </Box>
+                }
+
+              </Box>
+
+            </CardContent>
+            <CardActions>
+              <Box
+                flexDirection={'row'}
+                justifyContent={'left'}
+                alignItems={'left'}
+                display={'flex'}
+                width={'100%'}
+                gap={'5px'}
+              >
+                <Button variant='contained' size='small' color='primary'>Proyectos</Button>
+                <Button variant='outlined' size='small' color='error'>Eliminar</Button>
+
+                <IconButton onClick={(event) => handleClick(event, index)}> {/* Pasar el índice al hacer clic */}
+                  <MoreVert />
+                </IconButton>
+              </Box>
+            </CardActions>
+          </Card>
+        )}
+      </Box>
+    </Box>
   )
 }
 
